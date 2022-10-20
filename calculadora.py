d165e43cf9f7e3a6b2551df00befeb2fc70125a8
Matheus Alexandre <106098490+MatheusA199@users.noreply.github.com>
2022-10-20 18:25:01 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,62 +1,63 @@
-#Para editar a planilha
 import pandas as pd
-
-#Para editar a planilha
-from openpyxl import load_workbook
-
-#Para arredondar os números
+import openpyxl
+#from openpyxl import load_workbook, Workbook
 import math
-
-#Funções que a calculadora usa para ler a planilha e fazer as contas
 import funcao_calculadora
+import os
 
-#Função que contém a calculadora de carbono
+
 def calculadora_carbono():
-
-	#Para escrever a mensagem inicial
 	funcao_calculadora.mensagem_inicial()
 	
-	#Var que recebe a planilha respondida pelo usuário
-	planilha = load_workbook(filename = "planilha_medidas.xlsx")
-
-	#Var que recebe a folha da planilha respondida
+	planilha = openpyxl.load_workbook(filename = "planilha_medidas.xlsm")
 	sheet_range = planilha['Planilha1']
+	#sh2 = planilha['resultado']
 	#tempo_total = transformar_anos(sheet_range)
-
-	#Divisor para big int
 	divisores = 1000000000
-
-	#Valor que cada árvore converte de carbono
 	valor_arvore = 0.541
-
-	#Var que recebe a quantidade de carbono produzido em combustão móvel
 	carbono_movel = funcao_calculadora.calculo_combustao_movel(sheet_range)
 
-	#Var que receve a quantidade de carbono produzido em combustão aérea
 	carbono_aerea = funcao_calculadora.calculo_combustao_aerea(sheet_range)
 
-	#Var que recebe a quantidade de carbono produzido em consumo de energia elétrica 
 	carbono_eletrica = funcao_calculadora.energia_eletrica(sheet_range)
 
-	#Var que recebe a quantidade de carbono produzido em consumo de energia estacionária
 	carbono_estacionaria = funcao_calculadora.calculo_combustao_estacionaria(sheet_range)
 
-	#Var que recebe o total de carbono produzido pelo usuário
-	carbono_final = carbono_movel + carbono_aerea + carbono_eletrica + carbono_estacionaria
+	carbono_final_mes = carbono_movel + carbono_eletrica + carbono_estacionaria
 
+	carbono_final_ano = 12*(carbono_final_mes) + carbono_aerea
+	carbono_final_mes += carbono_aerea
+
+	total_arvores = math.ceil(carbono_final_mes/0.541)
+
+
+	carbono_final_ano = round(carbono_final_ano,2)
+	carbono_final_mes =  round(carbono_final_mes,2)
 
 	#carbono_final = carbono_final#/ divisores
 
-	#Var que recebe e arrenda para cima a quantidade de árvores que devem ser plantadas
-	total_arvores = math.ceil(carbono_final/valor_arvore)
+	#print(f'{carbono_final_mes:.3f} tCO2')
+	#print(f'O total de árvores que você deve plantar é {total_arvores}')
 
-	#Escrever o quanto de carbono o usuário produziu
-	print(f'{carbono_final:.3f} tCO2')
+	wb = openpyxl.load_workbook(filename = "resultado.xlsm")
+	sh1 = wb['resultado']
 
-	#Escrever quantas árvores ele deve plantar
-	print(f'O total de árvores que você deve plantar é {total_arvores}')
+	celula_quantidade_arvores = 'F6'
+	celula_quantidade_arvores_mes = 'F7'
+	celula_quantidade_carbono_ano = 'F8'
+
+	escrever_celula(total_arvores,sh1,celula_quantidade_arvores)
+	escrever_celula(carbono_final_mes,sh1,celula_quantidade_arvores_mes)
+	escrever_celula(carbono_final_ano,sh1,celula_quantidade_carbono_ano)
+
+	wb.save("resultado.xlsm")
+
+	os.startfile("C:\\Users\\alexa\\Desktop\\APS-UNIP\\resultado.xlsm")
+
+def escrever_celula(total_arvores,sh1, celula):
+	sh1[celula].value = total_arvores
 
 
-#Para chamar a função da calculadora de carbono e garantir que este é o arquivo principal que deve ser lido
+
 if (__name__ == '__main__'):
 	calculadora_carbono()
--- funcao_calculadora.py
+++ funcao_calculadora.py
@@ -1,113 +1,63 @@
-#Para arrendondar os números
 import math
-
-#Pode ser usado para converter a quantidade para anos
+from openpyxl import load_workbook, Workbook
 def transformar_anos(sheet_range):
 	quantidade_anos = int(sheet_range['C3'].value)
 	quantidade_tempo_total = quantidade_anos*12 + int(sheet_range['C4'].value)
 	return quantidade_tempo_total
 
-#Escrever mensagem inicial do programa
+
 def mensagem_inicial():
 	print('************************************************')
 	print('*Bem-vindo a calculadora de Crédito de Carbono!*')
 	print('************************************************')
 
-#Calcular combustão movel (Carros, ônibus, etc.)
 def calculo_combustao_movel(sheet_range):
-	#Variavel que recebe a quantidade de carbono produzido
+	sheet_range = sheet_range
 	carbono_final = 0
-
-	#Ideia para trabalhar com big int
 	#coeficente_carro = [208000000,237000000,306000000,306000000,92000000,98000000,140000000,350000000,306000000,27000000]
-
-	#Coeficiente de cada carro, multiplicado pelo divisor
 	coeficente_carro = [208,237,306,306,92,98,14,35,306,27]
 
-	#Divisores dos coeficientes
 	divisores = 1000000
-
-	#Recebe informações da tabela para o calculo, no caso, quilometro
 	lista_quilometro = ler_varias_celulas(sheet_range,'D',9,19)	
 
-	#Receber a quantidade de dcarbono produzido
 	carbono_final = somar_quantidade_carbono(coeficente_carro,lista_quilometro,carbono_final, divisores)
-
-	#Retorna a quantidade de carbono produzido como resposta
 	return carbono_final
 
-#Calcular combustão aerea (Viagens aéreas)
+
 def calculo_combustao_aerea(sheet_range):
-
-	#Coeficiente da viagem de avião
 	coeficiente_aviao = [130814]
-
-	#Var que recebe a quantidade de carbono produzido em combustão aérea
 	carbono_final = 0
-
-	#Divisor do coeficiente
 	divisores = 1000000000
-
-	#Recebe informação da tabela (Quantos quilometros o usuário andou de avião)
 	quantidade_quilometro = ler_varias_celulas(sheet_range,'G',9,10)
-
-	#Var que recebe a quantidade de carbono final produzido pelas viagens aéreas
 	carbono_final = somar_quantidade_carbono(coeficiente_aviao, quantidade_quilometro,carbono_final,divisores)
 
-	#Retorna como respostaa quantidade de carbono produzido por combustão aérea
 	return carbono_final
 
-#Calcular combustão estacionária (Gás encanado e de cozinha)
 def calculo_combustao_estacionaria(sheet_range):
-
-	#Var que guarda a quantidade de carbono produzido em combustão aérea
+	sheet_range = sheet_range
 	carbono_final = 0
-
-	#Coefientes de botijão e gás encanado
 	coeficente_estacionaria = [40000,2000]
-
 	#coeficente_estacionaria = [40000000,1997000]
-	#Divisores do coefiente
 	divisores = 1000000
-
-	#Var que recebe as informações do usuário da polanilha
-	lista_quantidade = ler_varias_celulas(sheet_range,'I',4,6)	
-
-	#Var que recebe a quantidade de carbono produzido em combustão estacionária
+	lista_quantidade = ler_varias_celulas(sheet_range,'I',4,5)	
 	carbono_final = somar_quantidade_carbono(coeficente_estacionaria, lista_quantidade,carbono_final,divisores)
 
-	#Retorna como resposta a quantidade de carbono produzido por combustão estacionária
 	return carbono_final
 
-#Calcular carbono produzido com consumo de energia elétrica
 def energia_eletrica(sheet_range):
-
-	#Var que guarda os coefientes de energia elétria(Amapá, Amazônas e Roraima; Demais estados; Internacional)
-	coeficiente_energia = [760,51,1500]
+	coeficiente_energia = [760,51] #1500
 	#coeficiente_energia = [760000000,51000000,1500000000]
 
-	#Var que armazena quantidade de carbono produzido 
 	carbono_final = 0
-
-	#Divisores de coefientes
 	divisores = 1000000
-
-	#Var que recebe as informações da tabela, em relação a quantidade de energia consumida
-	lista_gasto = ler_varias_celulas(sheet_range,'H',14,17)
-
-	#Var que recebe a quantidade de carbono produzido
+	lista_gasto = ler_varias_celulas(sheet_range,'H',14,16)
 	carbono_final = somar_quantidade_carbono(coeficiente_energia, lista_gasto,carbono_final,divisores)
 
-	#Retorna como repsosta a quantidade de carbono produzido em consumo de energia elétrica
 	return carbono_final
 
-#PAra ler as ceculas da planilha respondidas pelo usuário
+
 def ler_varias_celulas(sheet_range,letra,inicio,fim):
-
-	#Var que guarda os valores respondidos
 	lista_valores = []
-
-	#Laço que transformará o valor para float e append na lista
 	for i in range(inicio,fim):
 		try:
 			valor = float(sheet_range[f'{letra}{i}'].value)
@@ -116,15 +66,12 @@ def ler_varias_celulas(sheet_range,letra,inicio,fim):
 		except:
 			lista_valores.append(0)
 
-	#Retorna os valores respondidos
 	return lista_valores
 
-#Para somar/calcular a quantidade de carbono produzido
 def somar_quantidade_carbono(coeficiente,lista_valores,carbono_final, divisores):
-	#Guarda a quantidade de respostas
+
 	comprimento = len(lista_valores) 
 
-	#Laço que calcula o valor da quantidade de carbono produzido e guarda a resposta na var carbono_final
 	for i in range(comprimento):
 	
 		valores = lista_valores[i] * coeficiente[i] / divisores
@@ -132,8 +79,8 @@ def somar_quantidade_carbono(coeficiente,lista_valores,carbono_final, divisores)
 		valores_arredondado = math.ceil(valores)
 		carbono_final += valores
 		
-	#Escreve a quantidade de carbono produzido pela emissão em questão
 	print(carbono_final)
-
-	#Retorna como respota a quantidade de carbono produzido pela forma de emissão em questão
 	return carbono_final
+
+def escrever_celula(total_arvores,sh1, celula):
+	sh1[celula].value = total_arvores
